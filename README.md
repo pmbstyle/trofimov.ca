9d3b542ce3a7f1113eef215f0aff3d9a6a95f00f
Slava Trofimov <my@pmb.link>
2023-07-07 17:17:51 -0400

    Responsive adjustments, readme.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # trofimov.ca
 
-This template should help get you started developing with Vue 3 in Vite.
+Personal website
 
 ## Project Setup
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1500px'
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
