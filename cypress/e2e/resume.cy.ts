848359ed3d894c3836382e8c4233761ae6fdb1ea
Slava Trofimov <slava.trofimov@jobscan.co>
2024-04-04 11:20:01 -0400

    Update resume.cy.ts

--- cypress/e2e/resume.cy.ts
+++ cypress/e2e/resume.cy.ts
@@ -4,6 +4,6 @@ describe("Resume", () => {
     cy.get("div.item-resume > div.icon").click()
     cy.get("article h1").should("include.text", "Slava Trofimov")
     cy.get("div.close-window").click()
-    cy.get("div.game").should("not.exist")
+    cy.get("div.mockup-window").should("not.exist")
   })
 })
