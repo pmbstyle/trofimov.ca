4f4481bd1c8974e02b678acc1d03e01e05a20bb8
Slava Trofimov <slava@trofimov.ca>
2024-12-05 17:39:53 -0500

    window fullscreen

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import { defineEmits } from "vue";
+import { ref, defineEmits } from "vue";
 import CloseIcon from "@/assets/img/close.svg";
 import MinimizeIcon from "@/assets/img/minimize.svg";
 import ExpandIcon from "@/assets/img/expand.svg";
 
+const fullscreen = ref(false);
+
 const emit = defineEmits(["close"]);
+
+const toggleFullscreen = () => {
+  fullscreen.value = !fullscreen.value;
+};
 </script>
 <template>
   <div
-    class="mockup-window border bg-base-300 flex-1 flex flex-col mt-10 mb-5 ml-5 mr-5 drop-shadow-md absolute z-20"
+    class="mockup-window border bg-base-300 flex-1 flex flex-col drop-shadow-md absolute z-20"
+    :class="{ 'fullscreen mt-8 rounded-none': fullscreen, 'mt-10 mb-5 ml-5 mr-5': !fullscreen }"
   >
     <div class="window-header">
       <div class="close-window" @click="emit('close')">
@@ -17,8 +24,8 @@ const emit = defineEmits(["close"]);
       <div class="minimize-window" @click="emit('close')">
         <img :src="MinimizeIcon" />
       </div>
-      <div class="maximize-window">
-        <img :src="ExpandIcon" />
+      <div class="maximize-window" @click="toggleFullscreen">
+        <img :src="ExpandIcon" :class="{ 'rotate-180': fullscreen }" />
       </div>
     </div>
     <slot />
